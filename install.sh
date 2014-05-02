cb68e79e09c68c278831c0fa063e636a5b48bb19
tkyshm <tkyshm@gmail.com>
2014-05-02 21:30:53 +0900

    refresh install file

--- install.sh
+++ install.sh
@@ -1,15 +1,42 @@
 #!/bin/bash
 SCRIPT_DIR=$(cd $(dirname ${BASH_SOURCE:-$0}); pwd)
+
+##
+# to install
+# python-dev, liblua, lua, perl, libperl
+# mercurl
+##
+# Vim install(Ubuntu)
+hg clone https://vim.googlecode.com/hg/ vim
+cd vim/src
+./configure --with-features=huge \
+--enable-pythoninterp \
+--with-python-config-dir=/usr/lib/python2.7/config \
+--enable-fail-if-missing \
+--enable-multibyte \
+--enable-perlinterp \
+--enable-luainterp
+make
+sudo make install
+cd ../../
+
+# dot Files
+#cp dot.Xdefaults ~/.Xdefaults
+cp -r dot.zsh.d ~/.zsh.d
+cp dot.zshrc ~/.zshrc
+cp dot.vimrc ~/.vimrc
+cp dot.tmux.conf ~/.tmux.conf
+
 # Vimの初期設定
-#mkdir -p ~/.vim/bundle
-#git clone git://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
-#vim -c NeoBundleInstall
+mkdir -p ~/.vim/bundle
+git clone git://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
+vim -c NeoBundleInstall
 
 # ruby configuration
-#git clone https://github.com/sstephenson/rbenv.git ~/.rbenv
-#mkdir ~/.rbenv/plugins
-#git clone https://github.com/sstephenson/ruby-build.git .rbenv/plugins/ruby-build
-#git clone https://github.com/jf/rbenv-gemset.git .rbenv/plugins/rbenv-gemset
+git clone https://github.com/sstephenson/rbenv.git ~/.rbenv
+mkdir ~/.rbenv/plugins
+git clone https://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
+git clone https://github.com/jf/rbenv-gemset.git ~/.rbenv/plugins/rbenv-gemset
 
 # Fonts Configuration
 cd $SCRIPT_DIR
@@ -24,25 +51,24 @@ wget -O mig1m.zip 'http://sourceforge.jp/frs/redir.php?m=jaist&f=%2Fmix-mplus-ip
 unzip mig1m.zip
 rm mig1m.zip 
 sh ricty_generator.sh auto
-#cp Ricty-Regular.ttf Ricty-Bold.ttf ~/.vim/bundle/powerline/font
-#cd ~/.vim/bundle/powerline/font
-#fontforge -lang=py -script fontpatcher.py Ricty-Regular.ttf
-#fontforge -lang=py -script fontpatcher.py Ricty-Bold.ttf
+cp Ricty-Regular.ttf Ricty-Bold.ttf ~/.vim/bundle/powerline/font
+cd ~/.vim/bundle/powerline/font
+fontforge -lang=py -script fontpatcher.py Ricty-Regular.ttf
+fontforge -lang=py -script fontpatcher.py Ricty-Bold.ttf
 cp -f Ricty* ~/.fonts
 fc-cache -vf
 
 
 # Erlangの設定
-curl
-curl -O https://raw.github.com/spawngrid/kerl/master/kerl
+curl -O https://raw.githubusercontent.com/spawngrid/kerl/master/kerl
 mkdir ~/bin
 mv kerl ~/bin
 chmod a+x ~/bin/kerl
-iecho 'KERL_CONFIGURE_OPTIONS="--disable-hipe --enable-smp-support --enable-threads --enable-kernel-poll" ' > ~/.kerlrc
+echo 'KERL_CONFIGURE_OPTIONS="--disable-hipe --enable-smp-support --enable-threads --enable-kernel-poll" ' > ~/.kerlrc
 
-~/bin/kerl list release 
-~/bin/kerl update release 
-~/bin/kerl  release 
+~/bin/kerl list releases 
+~/bin/kerl update releases 
+#~/bin/kerl releases 
 
 # dot Files
 #cp dot.Xdefaults ~/.Xdefaults
