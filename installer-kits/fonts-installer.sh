f08bd3483c0f339e536ce97ea249dfbad1ac48a4
tkyshm <tkyshm@gmail.com>
2021-01-04 10:48:10 +0900

    polybar用のiconフォント、その他フォントを追加する

--- installer-kits/fonts-installer.sh
+++ installer-kits/fonts-installer.sh
@@ -6,3 +6,9 @@ mkdir -p ~/.fonts
 cp $SCRIPT_DIR/fonts/Inconsolata+for+Powerline.otf ~/.fonts
 cp $SCRIPT_DIR/fonts/PowerlineSymbols.otf ~/.fonts
 fc-cache -vf
+
+yay -S --noconfirm gohufont
+git clone https://github.com/stark/siji && cd siji
+./install.sh -d ~/.fonts
+cd -
+rm -rf siji
